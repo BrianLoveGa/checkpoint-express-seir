f9271b3dc2bfc2f092e7f745a09c6be86929ce25
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-03 11:08:17 -0400

    done for now far from finished

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/notes-checkpoint", {useNewUrlParser: true}).then((conn) => {
+mongoose.connect("mongodb://localhost/notes-checkpoint", {useUnifiedTopology: true}).then((conn) => {
 	console.log(`hooked it up mon 🌴 to mongodb on ${conn.connections[0].name} db`)
 })
 .catch(err => {
